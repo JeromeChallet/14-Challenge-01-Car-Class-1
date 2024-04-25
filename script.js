d82afe4755e9aeb0804bab797d9af0771fe28a42
AzureAD\JeromeChallet <jeromepchallet@gmail.com>
2024-04-25 13:19:10 +0900

    1

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ and log the new speed to the console
 3. Implement a 'brake' method that will decrease the car's speed by 5, and log 
 the new speed to the console
 4. Create 2 'Car' objects and experiment with calling 'accelerate' and 
-'brake' multiple times on each of them
+'brake' multiple times on each of themf
 Test data:
-§ Data car 1: 'BMW' going at 120 km/h
+§ Data car 1: 'BMW' going at 120 km/h 
 § Data car 2: 'Mercedes' going at 95 km/  
 */
 
